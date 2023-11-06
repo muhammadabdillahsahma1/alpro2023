3e9c89636c1ebf26215e10df3305a442a48eeb84
muhammadabdillahsahma1 <148075127+muhammadabdillahsahma1@users.noreply.github.com>
2023-11-06 12:03:36 +0700

    Delete tugas5 directory

--- tugas5/abdi5.py
+++
@@ -1,9 +0,0 @@
-if __name__ == "__main__":
-	tinggi = int(input('masukkan tinggi:' ))
-	
-	for x in range(1, tinggi + 1):
-		for y in range(1, x+1):
-			print('*', end='')
-		print()
-		
-	print('selesai')
