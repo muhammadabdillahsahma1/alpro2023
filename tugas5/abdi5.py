9151b08b9e4b9b7a3a010e963dcb813881780a1d
muhammadabdillahsahma1 <abdillah.22315025@umkendari.ac.id>
2023-11-06 20:45:33 +0700

    tugas 5 done

--- tugas5/abdi5.py
+++ tugas5/abdi5.py
@@ -2,7 +2,7 @@ if __name__ == "__main__":
 	tinggi = int(input('masukkan tinggi:' ))
 	
 	for x in range(1, tinggi + 1):
-		for y in range(1, x+1):
+		for y in range(tinggi-x+1):
 			print('*', end='')
 		print()
 		
